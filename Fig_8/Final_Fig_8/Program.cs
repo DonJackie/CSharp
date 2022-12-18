1f0015bde47a37ddaf9ac46c6cfe9b4aa114906b
donghyunlim <o12@kakao.com>
2022-12-18 19:12:14 +0900

    ForEachTest

--- Fig_8/Final_Fig_8/Program.cs
+++ Fig_8/Final_Fig_8/Program.cs
@@ -13,9 +13,9 @@ namespace Final_Fig_8
             int[] array = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
             int total = 0;
 
-            for (int counter=0; counter < array.Length; ++counter)
+            foreach( int number in array)
             {
-                total += array[counter];
+                total += number;
             }
             Console.WriteLine($"Total of array elements: {total}");
         }
