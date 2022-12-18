b2099f2bd0b34778fd0bf2d8eb24179ce440c9c9
donghyunlim <o12@kakao.com>
2022-12-18 17:00:45 +0900

    Optional parameter demonstration with method Power

---
+++ Final_Fig_7_15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Fig_7_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine($"Power(10)={Power(10)}");
+            Console.WriteLine($"Power(2,10) = {Power(2, 10)}");
+        }
+
+        static int Power(int baseValue, int exponentValue = 2)
+        {
+            int result = 1;
+            for(int i=1; i<= exponentValue; ++i)
+            {
+                result *= baseValue;
+            }
+            return result;
+        }
+    }
+}
