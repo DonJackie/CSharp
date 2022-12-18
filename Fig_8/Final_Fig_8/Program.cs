1d878cbe5b3930481e559d614d97e1d87c9338e2
donghyunlim <o12@kakao.com>
2022-12-18 19:10:14 +0900

    SumArray

--- Fig_8/Final_Fig_8/Program.cs
+++ Fig_8/Final_Fig_8/Program.cs
@@ -4,25 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Final_Fig_8_2
+namespace Final_Fig_8
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //int[] array = new int[5];
-            const int ArrayLength = 5;
-            int[] array = new int[ArrayLength];
+            int[] array = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+            int total = 0;
 
-            for(int counter = 0; counter < array.Length; ++counter)
+            for (int counter=0; counter < array.Length; ++counter)
             {
-                array[counter] = 2 + 2 * counter;
+                total += array[counter];
             }
-            Console.WriteLine($"{"Index"}{"Value",8}");
-            for(int counter=0; counter <array.Length; ++counter)
-            {
-                Console.WriteLine($"{counter,5}{array[counter],8}");//ArrayLength는 상수라 counter대신 사용될 수 없다.
-            }
+            Console.WriteLine($"Total of array elements: {total}");
         }
     }
 }
