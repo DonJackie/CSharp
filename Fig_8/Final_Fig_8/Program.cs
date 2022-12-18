152f7e1405bd05ad769287cc791d40f23568f86c
donghyunlim <o12@kakao.com>
2022-12-19 00:11:31 +0900

    Grade book 시험 나올 것 같음

--- Fig_8/Final_Fig_8/Program.cs
+++ Fig_8/Final_Fig_8/Program.cs
@@ -8,31 +8,110 @@ namespace Final_Fig_8
 {
     class Program
     {
-        static void Main(string[] args)
+        private int[] grades;
+        public string CourseName { get; }
+        public Program(string name, int[] gradesArray)
         {
-            int[] array = { 1, 2, 3, 4, 5 };
+            CourseName = name;
+            grades = gradesArray;
+        }
+        public void DisplayMessage()
+        {
+            Console.WriteLine(
+                $"Welcome to the grade book for \n{CourseName}!\n");
+        }
+        public void ProcessGrades()
+        {
+            OutputGrades();
+            Console.WriteLine($"\nCalss average is {GetAverage():F}");
 
-            Console.WriteLine("Effects of passing reference to entire array:");
-            Console.WriteLine("The values of the original array are:");
+            Console.WriteLine($"Lowest grade is {GetMinimum()}");
+            Console.WriteLine($"Highest grade is {GetMaximum()}\n");
 
-            foreach( var value in array)
-            {
-                Console.Write($"    {value}");
-            }
-            ModifyArray(array);
-            Console.WriteLine("\n\nThe values of the modified array are:");
-            foreach(var value in array)
-            {
-                Console.Write($"    {value}");
-            }
+            OutputBarChart();
         }
 
-        static void ModifyArray(int[] array2)
+        public int GetMinimum()
         {
-            for (var counter = 0; counter < array2.Length; ++counter)
+            var lowGrade = grades[0];
+            foreach(var grade in grades)
             {
-                array2[counter] *= 2;
+                if(grade < lowGrade)
+                {
+                    lowGrade = grade;
+                }
             }
+            return lowGrade;
+        }
+
+        public int GetMaximum()
+        {
+            var highGrade = grades[0];
+
+            foreach(var grade in grades)
+            {
+                if(grade > highGrade)
+                {
+                    highGrade = grade;
+                }
+            }
+            return highGrade;
+        }
+        public double GetAverage()
+        {
+            var total = 0.0;
+            foreach(var grade in grades)
+            {
+                total += grade;
+            }
+            return total / grades.Length;
+        }
+
+        public void OutputBarChart()
+        {
+            Console.WriteLine("grade distribution");
+            var frequency = new int[11];
+
+            foreach(var grade in grades)
+            {
+                ++frequency[grade / 10];
+            }
+            for (var count =0; count < frequency.Length; ++count)
+            {
+                if(count == 10)
+                {
+                    Console.Write("  100: ");
+                }
+                else
+                {
+                    Console.Write($"{count * 10:D2}-{count * 10 + 9:D2}: ");
+                }
+
+                for(var stars = 0; stars < frequency[count]; ++stars)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void OutputGrades()
+        {
+            Console.WriteLine("The grades are:\n");
+            for(var student=0; student < grades.Length; ++student)
+            {
+                Console.WriteLine(
+                    $"Student {student + 1,2}: {grades[student],3}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] gradesArray = { 87, 68, 97, 100, 83, 78, 85, 91, 76, 87 };
+
+            var myGradeBook = new Program(
+                "CS101 Introduction to C# Programming", gradesArray);
+            myGradeBook.DisplayMessage();
+            myGradeBook.ProcessGrades();
+
         }
     }
 }
