83bb759f40930451b4a48852993e53e4bf99e3d1
donghyunlim <o12@kakao.com>
2022-12-18 18:30:32 +0900

    Recursive Factorial method

---
+++ Final_Fig_7_17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Fig_7_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (long counter = 0; counter <= 10; ++counter)
+            {
+                Console.WriteLine($"{counter}! = {Factorial(counter)}");
+            }
+        }
+        static long Factorial(long a)
+        {
+            long result = 0;
+            if (a == 0 || a == 1)
+                return 1;
+            else result = a*Factorial(a-1);
+            return result;
+        }
+    }
+}
