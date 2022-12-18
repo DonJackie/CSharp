7484ab18ca0449ca44d6370cc707c73c2eb5986d
donghyunlim <o12@kakao.com>
2022-12-18 16:49:06 +0900

    [열거형소개]private enum MyEnum : typeName { }

---
+++ Final_Fig_7_8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Fig_7_8
+{
+    class Program
+    {
+        private enum Color : byte { RED, GREEN, BLUE}
+        static void Main(string[] args)
+        {
+            Color myColor1 = Color.RED;
+            Color myColor2 = (Color)2;
+
+            Console.WriteLine(myColor1);
+            Console.WriteLine(myColor2);
+
+        }
+    }
+}
