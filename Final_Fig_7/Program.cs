50f3a7008c6a23e634109cf93201adbfd88d5afd
donghyunlim <o12@kakao.com>
2022-12-18 15:12:04 +0900

    Maximum function

--- Final_Fig_7/Program.cs
+++ Final_Fig_7/Program.cs
@@ -10,6 +10,29 @@ namespace Final_Fig_7
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter first floating-point value : ");
+            double number1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter second floating-point value : ");
+            double number2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter third floating-point value : ");
+            double number3 = double.Parse(Console.ReadLine());
+
+            double result = Maximum(number1, number2, number3);
+
+            Console.WriteLine("Maximum is : " + result);
         }
+
+        static double Maximum(double n1, double n2, double n3)
+        {
+            double result=0;
+            if (n1 > n2) result = n1;
+            else result = n2;
+
+            if (n3 > result) result = n3;
+            return result;
+        }
+
     }
 }
